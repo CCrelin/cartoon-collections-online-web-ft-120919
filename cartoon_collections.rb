c8c22e383119f064164a6512d75054a547b8b9f9
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-12-31 23:35:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ counter += 1
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  
 end
 
 def long_planeteer_calls# code an argument here
